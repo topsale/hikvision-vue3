87a1857b486142ee2a931769a76192f637268a46
Lusifer <topsale@vip.qq.com>
2023-07-19 17:04:42 +0800

    update

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,3 +1,20 @@
 <template>
-    <div></div>
+    <div id="vs"></div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import Player from 'xgplayer';
+import 'xgplayer/dist/index.min.css';
+
+const initPlayer = () => {
+    let player = new Player({
+        id: 'vs',
+        url: 'http://localhost:8080/preview/rtsp.mp4'
+    });
+}
+
+onMounted(() => {
+    initPlayer()
+})
+</script>
